f69cff0acfb96c3093f81dda3b884c3b3c2b38c4
Rodrigodante11 <rodrigoaugusto839@gmail.com>
2023-03-09 17:40:38 -0300

    add token in back end project

--- core/serializers.py
+++ core/serializers.py
@@ -1,8 +1,20 @@
 from .models import Lista
 from rest_framework import serializers
+from .models import Item
+
+
+class ItemSerializer(serializers.HyperlinkedModelSerializer):
+    class Meta:
+        model = Item
+        fields = ['name', 'list', 'comprado', 'url']
 
 
 class ListaSerializer(serializers.HyperlinkedModelSerializer):
+    item_set = ItemSerializer(many=True)
+
     class Meta:
         model = Lista
-        fields = ['owner', 'name', 'url']
+        fields = ['owner', 'name', 'item_set' , 'url']
+
+
+
--- core/views.py
+++ core/views.py
@@ -1,10 +1,18 @@
 from django.shortcuts import render
-from .serializers import ListaSerializer
-from rest_framework import viewsets, permissions
-from .models import Lista
+from .serializers import ListaSerializer, ItemSerializer
+from rest_framework import viewsets, permissions, authentication
+from .models import Lista, Item
 
 
 class ListViewSet(viewsets.ModelViewSet):
     queryset = Lista.objects.all()
     serializer_class = ListaSerializer
+    permission_classes = [permissions.IsAuthenticated]  # acesso a pessoa autenticada
+    authentication_classes = [authentication.TokenAuthentication]
+
+
+class ItemViewSet(viewsets.ModelViewSet):
+    queryset = Item.objects.all()
+    serializer_class = ItemSerializer
     permission_classes = [permissions.IsAuthenticated] # acesso a pessoa autenticada
+    authentication_classes = [authentication.TokenAuthentication]
